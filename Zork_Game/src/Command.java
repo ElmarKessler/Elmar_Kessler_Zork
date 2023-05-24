fed1699febef356713276272433226fe0b1ee79e
Elmar KesslerElmar Kesslergit config --global user.name Elmar <elmar.kessler@noseryoung.com>
2023-05-24 12:14:35 +0200

    Update Zork
    Added more logic to user input
    Improved user input

--- Zork_Game/src/Command.java
+++ Zork_Game/src/Command.java
@@ -1,86 +1,169 @@
+import com.sun.source.tree.IfTree;
+
 import java.util.Scanner;
 
 public class Command {
-    public void userInput(){
+    private String firstWord;
+    private String secondWord;
+    private String command;
+
+    public void userInput() {
+        String[] keywords = {"go", "take", "use", "look", "inventory", "quit", "help"};
         Scanner scanner = new Scanner(System.in);
-        String command;
+        command = scanner.nextLine().toLowerCase();
+        String[] parts = command.split("\\s+");
 
-        // Game introduction
-        System.out.println("Welcome to Zork!");
-        System.out.println("You find yourself in a house.");
+        if (parts.length >= 1) {
+            firstWord = parts[0];
+        }
 
-        // Game loop
-        while (true) {
-            System.out.println();
-            System.out.print("> ");
-            command = scanner.nextLine().toLowerCase();
-
-            switch (command) {
+        if (parts.length >= 2) {
+            secondWord = parts[1];
+        }
+    }
+    public void InputHandler() {
+        Scanner scanner = new Scanner(System.in);
+            switch (firstWord) {
                 case "help":
                     System.out.println("Available commands: look, go, take, use, quit.");
                     break;
                 case "look":
-                    System.out.println("You look around the room. You see a leaflet in front of you.");
+                    String room = "room1";
+                    if (room.equals("room1")) {
+                        System.out.println("You look around the room. You see a leaflet in front of you.");
+                    }
                     break;
                 case "go":
-                    System.out.println("Where do you want to move to?");
-                    String specifiedInput = scanner.nextLine().toLowerCase();
-                    switch (specifiedInput) {
-                        case "forward":
-                            System.out.println("There are three rooms.");
-                            break;
-                        case "right":
-                            System.out.println("Here is a staircase that leads downstairs.");
-                            break;
-                        case "up":
-                            System.out.println("Here is an elevator that leads upstairs.");
-                            break;
-                        case "back":
-                            System.out.println("Here is the exit, blocked.");
-                            break;
-                        default:
-                            System.out.println("You can't go there.");
-                            break;
+                    if (secondWord == null) {
+                        System.out.println("Where do you want to go?");
+                        Scanner scanDirection = new Scanner(System.in);
+                        String specifiedInput = scanDirection.nextLine().toLowerCase();
+                        switch (specifiedInput) {
+                            case "north":
+                                System.out.println("Here is a door to another room");
+                                break;
+                            case "east":
+                                System.out.println("Here is a staircase that leads downstairs.");
+                                break;
+                            case "south":
+                                System.out.println("Here is an elevator that leads upstairs.");
+                                break;
+                            case "west":
+                                System.out.println("Here is the exit, blocked.");
+                                break;
+                            default:
+                                System.out.println("You can't go there.");
+                                break;
+                        }
+                    } else {
+                        switch (secondWord) {
+                            case "forward":
+                                System.out.println("Here is a door to another room");
+                                break;
+                            case "right":
+                                System.out.println("Here is a staircase that leads downstairs.");
+                                break;
+                            case "up":
+                                System.out.println("Here is an elevator that leads upstairs.");
+                                break;
+                            case "back":
+                                System.out.println("Here is the exit, blocked.");
+                                break;
+                            default:
+                                System.out.println("You can't go there.");
+                                break;
+                        }
                     }
                     break;
                 case "take":
-                    System.out.println("What do you want to take?");
+                    if (secondWord == null) {
+                        System.out.println("What do you want to take?");
+                        Scanner scanItem = new Scanner(System.in);
+                        String specifiedInput = scanItem.nextLine().toLowerCase();
+                        switch (specifiedInput) {
+                            case "item1":
+
+                                break;
+                            case "item2":
+                                System.out.println("Taken.");
+                                break;
+                            case "item3":
+                                System.out.println("Taken.");
+                                break;
+                            case "item4":
+                                System.out.println("Taken.");
+                                break;
+                            default:
+                                System.out.println("There is no such item.");
+                                break;
+                        }
+                    } else {
+                        switch (secondWord) {
+                            case "item1":
+                                System.out.println("Taken.");
+                                break;
+                            case "item2":
+                                System.out.println("Taken.");
+                                break;
+                            case "item3":
+                                System.out.println("Taken.");
+                                break;
+                            case "item4":
+                                System.out.println("Taken.");
+                                break;
+                            default:
+                                System.out.println("There is no such item.");
+                                break;
+                        }
+                    }
                     break;
                 case "use":
-                    System.out.println("What do you want to use?");
+                    if (secondWord == null) {
+                        System.out.println("What do you want to take?");
+                        Scanner scanUseItem = new Scanner(System.in);
+                        String specifiedInput = scanUseItem.nextLine().toLowerCase();
+                        switch (specifiedInput) {
+                            case "item1":
+                                System.out.println("Used.");
+                                break;
+                            case "item2":
+                                System.out.println("Used.");
+                                break;
+                            case "item3":
+                                System.out.println("Used.");
+                                break;
+                            case "item4":
+                                System.out.println("Used.");
+                                break;
+                            default:
+                                System.out.println("You can't use this item.");
+                                break;
+                        }
+                    } else {
+                        switch (secondWord) {
+                            case "item1":
+                                System.out.println("Used.");
+                                break;
+                            case "item2":
+                                System.out.println("Used.");
+                                break;
+                            case "item3":
+                                System.out.println("Used.");
+                                break;
+                            case "item4":
+                                System.out.println("Used.");
+                                break;
+                            default:
+                                System.out.println("You can't use this item.");
+                                break;
+                        }
+                    }
                     break;
                 case "quit":
                     System.out.println("Goodbye.");
                     System.exit(0);
                     break;
-                case "look up":
-                    System.out.println("There's nothing interesting above you.");
-                    break;
-                case "look down":
-                    System.out.println("You see the floor beneath your feet.");
-                    break;
-                case "go left":
-                    System.out.println("There's a wall blocking your way.");
-                    break;
-                case "take pen":
-                    System.out.println("You don't see a pen anywhere.");
-                    break;
-                case "use leaflet":
-                    System.out.println("The leaflet doesn't seem to have any use.");
-                    break;
-                case "go through door":
-                    System.out.println("There's no door in this room.");
-                    break;
-                case "take key":
-                    System.out.println("You don't see a key anywhere.");
-                    break;
-                case "use door":
-                    System.out.println("The door is locked.");
-                    break;
-                default:
-                    System.out.println("I don't recognize this command.");
-                    break;
             }
         }
     }
-}
+
--- Zork_Game/src/Game.java
+++ Zork_Game/src/Game.java
@@ -1,19 +1,15 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Game {
     public static void main(String[] args) {
 
 
-        System.out.printf("Willkommen zu Zork");
+        System.out.printf("Willkommen in Zork - \n\nGib deine Befehle ein, um voranzukommen und herauszufinden, was hier geschehen ist. Viel Glück!\n");
 
-        System.out.printf("Willkommen zu Zork - \n\nGib deine Befehle ein, um voranzukommen und herauszufinden, was hier geschehen ist. Viel Glück!\n");
-
-
-   Command command =new Command();
-   command.userInput();
+        while (true) {
+            Command command = new Command();
+            command.userInput();
+            command.InputHandler();
+        }
     }
-
     Item sword = new Item("Sword", "A sharp weapon for battles");
     Item potion = new Item("Health Potion", "Restores health points");
     Item key = new Item("Key", "Unlocks doors");
